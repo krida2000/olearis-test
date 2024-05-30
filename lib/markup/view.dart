60e765256b9a49b8b8608f87cdcac5ba61d9579d
krida2000 <andriykolesnuk2000@gmail.com>
2024-05-30 17:55:42 +0300

    Improve paddings on MarkupView

--- lib/markup/view.dart
+++ lib/markup/view.dart
@@ -42,56 +42,70 @@ class MarkupView extends StatelessWidget {
               ),
             ],
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset(
-                        'assets/olearis.png',
-                      ),
-                    ),
-                  ),
+          body: LayoutBuilder(builder: (_, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
                 ),
-                Obx(() {
-                  return Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: GridView.builder(
-                      itemCount: c.itemsCount.value,
-                      shrinkWrap: true,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 10,
-                        crossAxisSpacing: 10,
-                        childAspectRatio: 4,
-                      ),
-                      itemBuilder: (BuildContext context, int index) {
-                        return GridTile(
-                          child: Container(
-                            height: 10,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 2),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              'assets/olearis.png',
                             ),
-                            child: Center(child: Text('Item $index')),
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Obx(() {
+                        return SizedBox(
+                          height: ((constraints.maxWidth - 30) / 8 + 10) *
+                                  (c.itemsCount.value / 2).ceil() +
+                              10,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: GridView.builder(
+                              itemCount: c.itemsCount.value,
+                              shrinkWrap: true,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 10,
+                                crossAxisSpacing: 10,
+                                childAspectRatio: 4,
+                              ),
+                              itemBuilder: (BuildContext context, int index) {
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.black, width: 2),
+                                  ),
+                                  child: GridTile(
+                                    child: Center(child: Text('Item $index')),
+                                  ),
+                                );
+                              },
+                            ),
                           ),
                         );
-                      },
-                    ),
-                  );
-                }),
-              ],
-            ),
-          ),
+                      }),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
         );
       },
     );
